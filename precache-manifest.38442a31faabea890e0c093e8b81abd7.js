b916f7557a196af54ff09310767ce7367040bdf3
SirKoilok <53662293+SirKoilok@users.noreply.github.com>
2020-05-14 06:27:38 -0400

    Add files via upload

---
+++ precache-manifest.38442a31faabea890e0c093e8b81abd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39cf208d139960c76353",
+    "url": "/css/app.835e0858.css"
+  },
+  {
+    "revision": "eabe6a5eea9968adad95",
+    "url": "/css/chunk-vendors.6d527f87.css"
+  },
+  {
+    "revision": "ea3da7ca49792df5622bc9e9c934cc65",
+    "url": "/img/action_bar_logo.ea3da7ca.png"
+  },
+  {
+    "revision": "974cd8f6321286eeeda69f4056390902",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39cf208d139960c76353",
+    "url": "/js/app.034ff88e.js"
+  },
+  {
+    "revision": "eabe6a5eea9968adad95",
+    "url": "/js/chunk-vendors.da74e2c3.js"
+  },
+  {
+    "revision": "cf1ec32d419f4d2d9fe930d7d36db7ad",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
